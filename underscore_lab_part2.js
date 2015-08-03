5199e03bd4548f585dfaa59ad5187756ece38d06
Mary Mott <mott.maryt@gmail.com>
2015-08-03 16:48:21 -0700

    _.map created

--- underscore_lab_part2.js
+++ underscore_lab_part2.js
@@ -1,38 +1,39 @@
 //Part 2
 var _ = {
 	//each - iterates over an array and calls a given function with each element
-	each: function(arr, inputFunction) {
+	each: 	function(arr, inputFunction) {
 				for (var i = 0; i < arr.length; i++) {
 					inputFunction(arr[i]);
 				}
 			},
 	//compact - returns a new array with all provided undefined values removed
-	compact: function(arr) {
-				var noUndefined = [];
+	compact: 	function(arr) {
+					var noUndefined = [];
 
-				for (var i = 0; i < arr.length; i++) {
-					if (arr[i] !== undefined) {
-						noUndefined.push(arr[i]);
+					for (var i = 0; i < arr.length; i++) {
+						if (arr[i] !== undefined) {
+							noUndefined.push(arr[i]);
+						}
 					}
-				}
-				return noUndefined;
-	},
+					return noUndefined;
+				},
 	//map - returns a new array of values produced by running each element of 
 	//an array through a given function
-	map: function() {
+	map: 	function(arr, inputFunction) {
+				var values = [];
 
-	},
-	filter: function() {
+				for (var i = 0; i < arr.length; i++) {
+					values.push(inputFunction(arr[i]));
+				}
+				return values;
+			},
+	//filter - Looks through each value in the list, returning an array of all the values 
+	//that pass a truth test
+	filter: 	function() {
 
-	}
+				}
 
 }
-
-
-
-
-
-
 
 //each - iterates over an array and calls a given function with each element
 //_.each([1, 2, 3], alert);
@@ -44,9 +45,9 @@ console.log('compact test: ', _.compact([1, "hello", undefined, 3, undefined]));
 
 //map - returns a new array of values produced by running each element of an array through 
 //a given function
-_.map([1, 2, 3], function(num){ return num * 3; });
+console.log('_.map test: ', _.map([1, 2, 3], function(num){ return num * 3; }));
 //=> [3, 6, 9]
-_.map(["dogs", "before", "cats"], function(str){ return str.toUpperCase(); });
+console.log('_.map test: ', _.map(["dogs", "before", "cats"], function(str){ return str.toUpperCase(); }));
 //=> ["DOGS", "BEFORE", "CATS"]
 
 //filter - Looks through each value in the list, returning an array of all the values 
